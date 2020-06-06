745f292d9965f92ce0ad1b10bb9a782aaf22abb7
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2020-06-06 05:05:37 -0400

    embed cell changes. Now if the same key is provided they will overwrite

--- R/embed_cells.R
+++ R/embed_cells.R
@@ -9,8 +9,8 @@
 #' @param comp_select a vector of indices to use for embedding. NULL uses all, seq(1:10) would use first 10 sampleFactors withing the reducedDim(lem)
 #' @param tSNE_perp number of cells expressed above threshold for a given gene, 10-100 recommended.
 #' @param iterations The number of iterations for tSNE to perform.
-#' @param write_colDat whether or not to write the embedding to colData
-#' @param colDat_labs a vector of length 2 for the names of the X and Y colnames within colData()
+#' @param write_colData whether or not to write the embedding to colData
+#' @param colData_labs a vector of length 2 for the names of the X and Y colnames within colData()
 #' @param seed For tSNE, the seed. Can set to NULL if desired.
 #' @param print_progress will print progress if TRUE
 
@@ -21,26 +21,35 @@
 #'
 embed_cells <- function(input,
                         lem,
-                        embedding_key,
+                        embedding_key = NULL,
                         method = "tSNE",
                         comp_select = NULL,
                         tSNE_perp = 30,
                         iterations = 500,
-                        write_colDat = T,
-                        colDat_labs = c("x","y"),
+                        write_colData = T,
+                        colData_labs = NULL,
                         seed = 100,
                         print_progress = T){
 
   if(!(lem %in% reducedDimNames(input))){
     stop(paste0("LEM not found, LEMs available are ", paste0(reducedDimNames(input), collapse = ", ")))
   }
+
+  if(is.null(embedding_key)){
+    embedding_key <- method
+  }
+
+  if(is.null(colData_labs)){
+    colData_labs <- paste0(method, c("_x", "_y"))
+  }
+
   input_lem <- reducedDim(input, lem)
   embed_met <- metadata(input_lem)
   tsne_input <- sampleFactors(input_lem)
 
   if(!is.null(comp_select)){
     if(max(comp_select) > ncol(tsne_input)){
-      print(paste0("lem embedding only contains "), ncol(tsne_input), " dimensions. max(comp_select) must be less than this value")
+      print(paste0("lem embedding only contains "), ncol(tsne_input), " dimensions. max(comp_select) must be less than this value.")
     }
     tsne_input <- tsne_input[,comp_select]
   } else {
@@ -62,17 +71,18 @@ embed_cells <- function(input,
     embedding[,"x"] <-  embedding[,"x"]/max(embedding[,"x"])
     embedding[,"y"] <-  abs(min(embedding[,"y"]))+embedding[,"y"]
     embedding[,"y"] <-  embedding[,"y"]/max(embedding[,"y"])
+
+    colnames(embedding) <- colData_labs
   }
 
-  if(write_colDat){
-    colData(input)[colDat_labs[1]] <- embedding[,"x"]
-    colData(input)[colDat_labs[2]] <- embedding[,"y"]
+  if(write_colData){
+    colData(input)[colData_labs[1]] <- embedding[,colData_labs[1]]
+    colData(input)[colData_labs[2]] <- embedding[,colData_labs[2]]
   }
-
 
   ####
 
-  parameters <- list(lem, method, embedding_key, comp_select, tSNE_perp, iterations, write_colDat, colDat_labs, seed)
+  parameters <- list(lem, method, embedding_key, comp_select, tSNE_perp, iterations, write_colData, colData_labs, seed)
   names(parameters) <- c("lem", "method", "embedding_key", "comp_select", "tSNE_perp", "iterations", "write_colDat", "colDat_labs", "seed")
 
   embed_metadat <- list(embedding, parameters)
@@ -80,13 +90,11 @@ embed_cells <- function(input,
   embed_metadat <- list(embed_metadat)
   names(embed_metadat) <- embedding_key
 
-  ### Combining with existing LEM metadata
-  str(embed_metadat) # the embedding
-  str(embed_met) # the lem
-
-
-  embed_met <- append(embed_met, embed_metadat)
-  str(embed_met)
+  if(embedding_key %in% embeddings(input)$embedding_key){
+    metadata(reducedDim(input, lem))[["embeddings"]][embedding_key] <- embed_metadat
+  } else {
+    metadata(reducedDim(input, lem))[["embeddings"]] <- append(metadata(reducedDim(input, lem))[["embeddings"]], embed_metadat)
+  }
 
   return(input)
 
